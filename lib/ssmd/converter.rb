c30e14faf0b30e5927ef1763e3871c788762ac56
Markus Kahl <machisuji@gmail.com>
2017-05-12 14:50:41 +0100

    started explicit annotation

--- lib/ssmd/converter.rb
+++ lib/ssmd/converter.rb
@@ -20,7 +20,45 @@ class Converter
     Converter.new input.gsub(/@(\w+)/, '<mark name="\1"/>')
   end
 
+  def explicit_annotation
+
+  end
+
   def output
     "<speak>#{input.strip}</speak>"
+  end
+end
+
+class AnnotationProcessor
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  ##
+  # Matches explicitly annotated sections.
+  # For example:
+  #
+  #     [Guardians of the Galaxy](en-GB, v: +4dB, p: -3%)
+  def self.regex
+    %r{
+      \A
+      \[                              # opening text
+        ([^\]]+)                      # annotated text
+      \]                              # closing text
+      \(                              # opening annotations
+        ((?:
+          (?:
+            (?:#{language_regex})  # language annotation
+          )(?:,\s?)?
+        )+)
+      \)                              # closing annotations
+      \Z
+    }x
+  end
+
+  def self.language_regex
+    /[a-z]{2}(?:-[A-Z]{2})?/
   end
 end

98c5b12b1144aac210343789966288e49df5d44a
Markus Kahl <machisuji@gmail.com>
2017-05-16 00:53:05 +0100

    ensure whitespace around silent prosody too

--- lib/ssmd/processors/prosody_processor.rb
+++ lib/ssmd/processors/prosody_processor.rb
@@ -57,43 +57,66 @@ module SSMD::Processors
         }x
       end
 
+      ##
+      # Match example: ~silent~
       def silent_volume
-        /~#{content('silent')}~/
+        /#{ws_start}~#{content('silent')}~#{ws_end}/
       end
 
+      ##
+      # Match example: --extra soft-- or -soft-
       def soft_volume
         /(?:#{ws_start}--#{content('x-soft')}--#{ws_end})|(?:#{ws_start}-#{content('soft')}-#{ws_end})/
       end
 
+      ##
+      # Match example: ++extra loud or +loud+
       def loud_volume
         /(?:#{ws_start}\+\+#{content('x-loud')}\+\+#{ws_end})|(?:#{ws_start}\+#{content('loud')}\+#{ws_end})/
       end
 
+      ##
+      # Match example: <<extra slow<< or <slow<
       def slow_rate
         /(?:#{ws_start}&lt;&lt;#{content('x-slow')}&lt;&lt;#{ws_end})|(?:#{ws_start}&lt;#{content('slow')}&lt;#{ws_end})/
       end
 
+      ##
+      # Match example: >>extra fast>> or >fast>
       def fast_rate
         /(?:#{ws_start}&gt;&gt;#{content('x-fast')}&gt;&gt;#{ws_end})|(?:#{ws_start}&gt;#{content('fast')}&gt;#{ws_end})/
       end
 
+      ##
+      # Match example: __extra low__ or _low_
       def low_pitch
         /(?:#{ws_start}__#{content('x-low')}__#{ws_end})|(?:#{ws_start}_#{content('low')}_#{ws_end})/
       end
 
+      ##
+      # Match example: ^^extra high^^ or ^high^
       def high_pitch
         /(?:#{ws_start}\^\^#{content('x-high')}\^\^#{ws_end})|(?:#{ws_start}\^#{content('high')}\^#{ws_end})/
       end
 
+      ##
+      # Matches either a single char or a longer string starting and ending
+      # with a non-whitespace char.
       def content(name = nil)
         id = name ? "?<#{name}>" : ""
         /(#{id}(?:[^\s])|(?:[^\s].*[^\s]))/
       end
 
+      ##
+      # Matches the beginning of the input or a whitespace char via lookbehind,
+      # meaning it's excluded from the match result.
       def ws_start
         /(?<=(\A)|(?:\s))/
       end
 
+      ##
+      # Matches the end of the input or a whitespace char via lookahead,
+      # meaning it's excluded from the match result.
       def ws_end
         /(?=(\Z)|(?:\s))/
       end
